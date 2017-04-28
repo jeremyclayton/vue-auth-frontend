534efb36e96890c8825fbbecd7a7d7a9ac4c9bb0
jeremyclayton <jeremy.clayton.91@gmail.com>
2017-04-28 15:08:31 -0600

    added components and thier routes

---
+++ ultimate-startup-battle/src/components/AppNav.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="nav navbar-default">
+        <div class="navbar-header">
+            <router-link to="/" class="navvar-brand"> The Ultimate Startup Battle Ground</router-link>
+        </div>
+    </nav>
+    <ul class="nav navbar-nav navnar-right">
+        <li>
+            <button @click="handleLogout()"type="button" class="btn btn-danger log"name="button">Log Out</button>
+            <button @click="handeLogin()"type="button" class="btn btn-info log"name="button">Log In</button>
+
+        </li>
+
+    </ul>
+</template>
+
+<script>
+import { isLoggedIn, login, logout } from '../../utils/auth';
+
+export default {
+  name: 'app-nav',
+  methods: {
+    handleLogin() {
+      login();
+    },
+    handleLogout() {
+      logout();
+    },
+    isLoggedIn() {
+      return isLoggedIn();
+    },
+  },
+};
+
+</script>
+
+<style scoped>
+.navbar-right { margin-right: 0px !important}
+
+.log {
+  margin: 5px 10px 0 0;
+}
+</style>
